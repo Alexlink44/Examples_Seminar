3cee1c8898236fedceb0f9ea4b7dd659732e766d
Alex.link <alex.link_mail@e1.ru>
2021-11-25 13:45:33 +0500

    add Example 006

---
+++ Example006/Program.cs
@@ -0,0 +1,14 @@
+﻿//Выяснить является ли число чётным
+double number = new Random().Next(1, 1001);
+Console.WriteLine(number);
+double rem = number % 2;
+if (rem == 0)
+{
+Console.WriteLine(number);
+Console.WriteLine(" - число является чётным");
+}
+else
+{
+Console.WriteLine(number);
+Console.WriteLine(" - число НЕ является чётным");
+}
