62905f6f8d22b8d8156d98ae431628bb33703adf
Alex.link <alex.link_mail@e1.ru>
2021-11-27 21:20:26 +0500

    add Example 008

---
+++ Example008/Program.cs
@@ -0,0 +1,50 @@
+﻿//Показать четные числа от 1 до N
+void FillArray(int[] naturalNumbrs)
+{
+    int lengthN = naturalNumbrs.Length;
+    for (int i = 0; i < lengthN; i++)
+    {
+        naturalNumbrs[i] = new Random().Next(1, 1001);
+    }
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.Write(col[position]);
+        position++;
+        if (position < count) Console.Write(", ");
+        else Console.WriteLine(".");
+    }
+}
+
+void PrintArrayResult(int[] numbers)
+{
+    int count = numbers.Length;
+    Console.WriteLine();
+    for (int p = 0; p < count; p++)
+    {
+        if (numbers[p] % 2 == 0)
+        {
+            Console.Write($"{numbers[p]}");
+        }
+        if (p == count - 1)
+        {
+            Console.Write(".");
+            break;
+        }
+        if (numbers[p] % 2 == 0)
+        {
+            Console.Write(", ");
+        }
+    }
+}
+
+int[] array = new int[10];
+
+FillArray(array);
+PrintArray(array);
+PrintArrayResult(array);
