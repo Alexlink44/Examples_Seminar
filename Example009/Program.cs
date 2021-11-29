4f78978a4b0794b551fc633ec0d981fb4d0d7496
Alex.link <alex.link_mail@e1.ru>
2021-11-29 13:36:30 +0500

    add Example 009

---
+++ Example009/Program.cs
@@ -0,0 +1,9 @@
+﻿//Показать последнюю цифру трёхзначного числа
+//int NewNumber = new Random().Next(100, 1000);
+//string Number = NewNumber.ToString();
+//Console.Write($"{Number[2]}");
+
+int NewNumber = new Random().Next(100, 1000);
+Console.WriteLine("Number - {0}", NewNumber);
+int Res = NewNumber % 10;
+Console.WriteLine("Последняя цифра числа - {0}", Res);
