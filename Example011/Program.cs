1fcd5a939f0035066b1b0b53ff225c3a46a8c99d
Alex.link <alex.link_mail@e1.ru>
2021-11-29 14:17:22 +0500

    add Example 011

---
+++ Example011/Program.cs
@@ -0,0 +1,18 @@
+﻿//Дано число из отрезка [10, 99]. Показать наибольшую цифру числа
+
+int Number = new Random().Next(10, 100);
+Console.WriteLine(Number);
+int A = Number % 10;
+int B = Number / 10;
+if (A == B) Console.Write($"Число {Number} симметричное");
+else
+{
+    if (B > A)
+    {
+        Console.WriteLine($"Цифра второго порядки числа {Number} наибольшая");
+    }
+    else
+    {
+     Console.WriteLine($"Цифра первого порядки числа {Number} наибольшая");
+    }
+}
