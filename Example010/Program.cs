b4d4f6688e93fad788777fbc093a9899da355a4f
Alex.link <alex.link_mail@e1.ru>
2021-11-29 13:48:47 +0500

    add Example 010

---
+++ Example010/Program.cs
@@ -0,0 +1,7 @@
+﻿//Показать вторую цифру трёхзначного числа
+
+int NewNumber = new Random().Next(100, 1000);
+Console.WriteLine("Number - {0}", NewNumber);
+int Temp = NewNumber / 10;
+int Res = Temp % 10;
+Console.WriteLine("Вторая цифра трёхзначного числа - {0}", Res);
