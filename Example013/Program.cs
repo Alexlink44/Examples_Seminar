e13b74e183053688cae8de8257e8a579228e4c00
Alex.link <alex.link_mail@e1.ru>
2021-12-02 00:32:08 +0500

    add Example 013

---
+++ Example013/Program.cs
@@ -0,0 +1,10 @@
+﻿// Выяснить, кратно ли число заданному, если нет, вывести остаток.
+void Method(int given)
+{
+    Console.Write("Введите число ");
+    int numb = int.Parse(Console.ReadLine());
+    if (given % numb == 0) Console.WriteLine($"Число {numb} кратно заданному");
+    else Console.WriteLine(given % numb);
+}
+
+Method(3);
