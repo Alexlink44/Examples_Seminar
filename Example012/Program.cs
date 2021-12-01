4ee45146a39d7ea41b2313ef247f2c1fac11a00e
Alex.link <alex.link_mail@e1.ru>
2021-12-02 00:14:55 +0500

    add Example 012

---
+++ Example012/Program.cs
@@ -0,0 +1,8 @@
+﻿//Удалить вторую цифру трёхзначного числа
+int NewNumber = new Random().Next(100, 1000);
+Console.WriteLine("Number - {0}", NewNumber);
+int Temp1 = NewNumber / 100;
+int Temp2 = NewNumber % 10;
+Temp1 = Temp1 * 10;
+int Result = Temp1 + Temp2;
+Console.WriteLine(Result);
