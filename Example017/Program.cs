8a5880b040722324576afb9754e3ade8a54732e2
Alex.link <alex.link_mail@e1.ru>
2021-12-02 13:48:43 +0500

    add Example 017

---
+++ Example017/Program.cs
@@ -0,0 +1,12 @@
+﻿// По двум заданным числам проверить является ли одно квадратом другого
+
+Console.Write("Enter number: ");
+int num1 = int.Parse(Console.ReadLine());
+Console.Write("Enter number 2: ");
+int num2 = int.Parse(Console.ReadLine());
+if ((num1 / num2) == num2) Console.WriteLine("Первое число является квадратом второго");
+else
+{
+    if ((num2 / num1) == num1) Console.WriteLine("Второе число является квадратом первого");
+    else Console.WriteLine("Ни одно из чисел не является квадратом другого");
+}
