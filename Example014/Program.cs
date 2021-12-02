239f21883875d2e0795f3b74f95019886e8b53f0
Alex.link <alex.link_mail@e1.ru>
2021-12-02 12:23:19 +0500

    add Example 014

---
+++ Example014/Program.cs
@@ -0,0 +1,13 @@
+﻿// Найти третью цифру числа или сообщить, что её нет
+
+Console.Write("Enter number: ");
+string num = Console.ReadLine();
+int length = num.Length;
+if (length < 3)
+{
+    Console.WriteLine("Третьей цифры не существует");
+}
+else
+{
+Console.WriteLine(num[2]);
+}
