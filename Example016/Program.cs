d6ab3fd377f37d4144e44eea01d6c75499e1729e
Alex.link <alex.link_mail@e1.ru>
2021-12-02 13:29:42 +0500

    add Example 016

---
+++ Example016/Program.cs
@@ -0,0 +1,9 @@
+﻿// Дано число обозначающее день недели. Выяснить является номер дня недели выходным
+Console.Write("Enter number: ");
+int num = int.Parse(Console.ReadLine());
+if (num == 6) Console.WriteLine("Этот день недели - выходной");
+else
+{
+    if (num == 7) Console.WriteLine("Этот день недели - выходной");
+    else Console.WriteLine("Этот день недели - НЕ является выходным");
+}
