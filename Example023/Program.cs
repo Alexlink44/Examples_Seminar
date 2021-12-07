5f987ffa8e6e318b41b9cb9f8a93450d2095e412
Alex.link <alex.link_mail@e1.ru>
2021-12-08 01:49:54 +0500

    add Example 023

---
+++ Example023/Program.cs
@@ -0,0 +1,23 @@
+﻿// Показать таблицу квадратов чисел от 1 до N.
+Console.Write("Задайте N: ");
+int table = int.Parse(Console.ReadLine());
+int column = table / 10;
+for (int k = 0; k <= 9; k++) {Console.Write($"       {k}");}
+Console.WriteLine();
+Console.WriteLine();
+for (int i = 0; i <= column; i++)
+{
+    Console.Write($"{i}");
+    for (int j = 0; j <= 9; j++)
+    {
+        int indentSize = 9;
+        int number = ((i * 10) + j) * ((i * 10) + j);
+        string numberlength = Convert.ToString(number);
+        int length = numberlength.Length;
+        indentSize = indentSize - length;
+        for (int h = 1; h < indentSize; h++){Console.Write(" ");}
+        Console.Write(number);
+        }
+    Console.WriteLine();
+    Console.WriteLine();
+}
