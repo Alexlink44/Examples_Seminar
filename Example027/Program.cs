b8e40c8dc3da392b324a93ddaab2c1767d1b6582
Alex.link <alex.link_mail@e1.ru>
2021-12-09 01:08:53 +0500

    add Example 027

---
+++ Example027/Program.cs
@@ -0,0 +1,15 @@
+﻿// Определить количество цифр в числе.
+
+Console.Write("Enter number: ");
+double number = Convert.ToDouble(Console.ReadLine());
+if (number < 0) {number = number * (-1);}
+while (number % 1 > 0) {number = number * 10;}
+Console.WriteLine(number);
+int i = 0;
+int num = Convert.ToInt32(number);
+while (num > 0)
+{
+    num = num / 10;
+    i++;
+}
+Console.Write($"Количество цифр в числе равно {i}");
