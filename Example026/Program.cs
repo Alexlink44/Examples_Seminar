c7fb43861623b7ab10f40feb830217c8331cd2a8
Alex.link <alex.link_mail@e1.ru>
2021-12-09 00:23:50 +0500

    add Example 026

---
+++ Example026/Program.cs
@@ -0,0 +1,12 @@
+﻿// Возведите число А в натуральную степень B используя цикл.
+
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+Console.Write("Определите степень возведения числа: ");
+int degree = int.Parse(Console.ReadLine());
+int total = number;
+for (int i = 1; i < degree; i++)
+{
+    total = number * total;
+}
+Console.Write(total);
