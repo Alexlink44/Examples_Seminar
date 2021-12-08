498e6f81e07128d6db345d22c75d90bf4ccb659e
Alex.link <alex.link_mail@e1.ru>
2021-12-08 23:50:46 +0500

    add Example 024

---
+++ Example024/Program.cs
@@ -0,0 +1,18 @@
+﻿// Найти кубы чисел от 1 до N
+
+Console.Write("Задайте N: ");
+int number = int.Parse(Console.ReadLine());
+int k = 0;
+for (int i = 1; i <= number; i++)
+{
+    int indentSize = 9;
+    string numberlength = Convert.ToString(i);
+    int j = i * i * i;
+    string lengthOfNumber = Convert.ToString(j);
+    int length = numberlength.Length + lengthOfNumber.Length;
+    indentSize = indentSize - length;
+    for (int h = 1; h < indentSize; h++) { Console.Write(" "); }
+    Console.Write($"{i}^3 = {j}");
+    k++;
+    if (k == 5) { Console.WriteLine(); k = 0; }
+}
