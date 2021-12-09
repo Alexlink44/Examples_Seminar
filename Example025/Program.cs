a1e092a5555a027582ab05642316f1480c9d1d54
Alex.link <alex.link_mail@e1.ru>
2021-12-10 02:08:41 +0500

    add Example 025

---
+++ Example025/Program.cs
@@ -0,0 +1,48 @@
+﻿// Найти сумму чисел от 1 до А.
+
+Console.Write("Enter number A: ");
+double number = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Введите шаг: ");
+double step = Convert.ToDouble(Console.ReadLine());
+
+// double len = ((number - 1) / step) + 1;
+// Console.WriteLine(len);
+// len = Math.Truncate(len);
+// int length = Convert.ToInt32(len);
+// Console.WriteLine(length);
+// if ((number - 1) % step == 0) {length = length - 1;}
+// Console.WriteLine(length);
+
+
+// double set = 1;
+// double k = 1;
+
+// for (int i = 1; i <= length; i++)
+// {
+//     if (i == length) {set = set + number;}
+//     else 
+//     {
+//         k = k + step;
+//         Console.WriteLine(k);
+//         set = set + k;
+//     }
+// }
+
+// Console.WriteLine(set);
+
+/////////////////////////////////////////////////////////////////
+
+double set = 0;
+double k = 1;
+
+while (k < number)
+{
+    set = set + k;
+    k = k + step;
+    Console.WriteLine(k);
+}
+
+set = set + number;
+
+Console.WriteLine(set);
--- Example028/Program.cs
+++ Example028/Program.cs
@@ -14,8 +14,9 @@ int[] Array(string number)
     for (int i = 0; i < length; i++)
     {
         if(number[i] == value) numb[i] = 0;
-        else numb[i] = (int)(number[i] - '0');
-        //(int)Char.GetNumericValue(number[i]);
+        else numb[i] = (int)Char.GetNumericValue(number[i]);
+        //(int)(number[i] - '0');
+        //
         //Convert.ToInt32(number[i]) -48; ---
     }
     return numb;
