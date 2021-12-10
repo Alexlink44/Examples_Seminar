e7b3b2ccbc08af6c7321d9a9d3e0b1f429fb6f00
Alex.link <alex.link_mail@e1.ru>
2021-12-10 17:00:07 +0500

    fix Example 031, add Example 032

--- Example031/Program.cs
+++ Example031/Program.cs
@@ -6,8 +6,10 @@ for (int i = 0; i < 8; i++)
     array[i] = new Random().Next(-100, 100);
 }
 for (int i = 0; i < 8; i++)
-{
-    if (i == 0) {Console.Write("{"); Console.Write($"{array[i]}, ");}
-    if (i == 7) {Console.Write($"{array[i]}"); Console.Write("}");}
+{   if (i == 0 || i == 7)
+    {
+        if (i == 0) {Console.Write("{"); Console.Write($"{array[i]}, ");}
+        if (i == 7) {Console.Write($"{array[i]}"); Console.Write("}");}
+    }
     else {Console.Write($"{array[i]}, ");}
 }
---
+++ Example032/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задать массив из 8 элементов, заполненных нулями и единицами вывести их на экран.
+
+string[] array = new string[8];
+for (int i = 0; i < 8; i++)
+{
+    int k = new Random().Next(1, 6);
+    string temparray = String.Empty;
+    for (int j = 0; j < k; j++)
+    {
+        int h = new Random().Next(1, 3);
+        if(h == 1) temparray = temparray + '1';
+        else temparray = temparray + '0';
+    }
+    // Console.WriteLine(temparray);
+
+    array[i] = temparray;
+    // Console.WriteLine(array[i]);
+}
+
+
+
+
+
+for (int i = 0; i < 8; i++)
+{   if (i == 0 || i == 7)
+    {
+        if (i == 0) {Console.Write("{"); Console.Write($"{array[i]}, ");}
+        if (i == 7) {Console.Write($"{array[i]}"); Console.Write("}");}
+    }
+    else {Console.Write($"{array[i]}, ");}
+}
