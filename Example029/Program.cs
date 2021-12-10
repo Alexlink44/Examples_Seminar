c3766019e93170b61ffe64c06d13616a3ec0f343
Alex.link <alex.link_mail@e1.ru>
2021-12-10 15:18:56 +0500

    add Example 029

---
+++ Example029/Program.cs
@@ -0,0 +1,13 @@
+﻿// Написать программу вычисления произведения чисел от 1 до N.
+
+Console.Write("Введите конечное число N: ");
+int number = int.Parse(Console.ReadLine());
+int result = 1;
+int temp = 1;
+while (temp < number)
+{
+    temp = temp + 1;
+    result = result * temp;
+}
+
+Console.WriteLine(result);
