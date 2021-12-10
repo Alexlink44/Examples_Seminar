63bf877d6d4118e00cbdd716399d535aa60c91a9
Alex.link <alex.link_mail@e1.ru>
2021-12-10 16:13:08 +0500

    add Example 031

---
+++ Example031/Program.cs
@@ -0,0 +1,13 @@
+﻿//Задать массив из 8 элементов и вывести их на экран
+
+int[] array = new int[8];
+for (int i = 0; i < 8; i++)
+{
+    array[i] = new Random().Next(-100, 100);
+}
+for (int i = 0; i < 8; i++)
+{
+    if (i == 0) {Console.Write("{"); Console.Write($"{array[i]}, ");}
+    if (i == 7) {Console.Write($"{array[i]}"); Console.Write("}");}
+    else {Console.Write($"{array[i]}, ");}
+}
