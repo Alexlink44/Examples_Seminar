7324410cfde1b418c71ad5add46de0c38f1c5334
Alex.link <alex.link_mail@e1.ru>
2021-12-10 15:56:02 +0500

    add Example 030

---
+++ Example030/Program.cs
@@ -0,0 +1,30 @@
+﻿// Показать кубы чисел, заканчивающихся на четную цифру.
+
+int[] Numbers(int amount)
+{
+    int[] numbers = new int[amount];
+    for (int i = 0; i < amount; i++)
+    {
+        numbers[i] = new Random().Next(1, 50);
+        Console.Write($"{numbers[i]}   ");
+    }
+    return numbers;
+}
+
+void EvenCubes(int[] numbers)
+{
+    int length = numbers.Length;
+    for (int i = 0; i < length; i++)
+    {
+        int number = numbers[i] * numbers[i] * numbers[i];
+        if (numbers[i] % 2 == 0 && numbers[i] % 10 != 0)
+        {
+            Console.WriteLine($"{numbers[i]}^3 = {number}");
+        }
+    }
+}
+
+int[] numbers = Numbers(10);
+Console.WriteLine();
+Console.WriteLine();
+EvenCubes(numbers);
