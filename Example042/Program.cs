55e38bec22b6ccd4d1a23d967c09d0fe62764aa1
Alex.link <alex.link_mail@e1.ru>
2021-12-15 00:50:44 +0500

    add Example 042

---
+++ Example042/Program.cs
@@ -0,0 +1,16 @@
+﻿//Определить сколько чисел больше 0 введено с клавиатуры
+
+int[] numbers = new int[10];
+for (int j = 0; j < 10; j++)
+{
+    Console.Write($"Введите число {j + 1}: ");
+    numbers[j] = Convert.ToInt32(Console.ReadLine());
+}
+int length = numbers.Length;
+int amount = 0;
+for (int i = 0; i < length; i++)
+{
+    if (numbers[i] > 0) {amount = amount + 1;}
+}
+Console.WriteLine();
+Console.Write($"{amount} чисел больше 0 введено с клавиатуры");
