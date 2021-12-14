26237276c469d9ec9d53d42331bcb8bfd12daf22
Alex.link <alex.link_mail@e1.ru>
2021-12-15 00:31:28 +0500

    add Example 041

---
+++ Example041/Program.cs
@@ -0,0 +1,22 @@
+﻿//Выяснить являются ли три числа сторонами треугольника
+
+int Sides()
+{
+    int number = new Random().Next(1, 20);
+    Console.Write($"{number}  ");
+    return number;
+}
+
+void Triangle(int a, int b, int c)
+{
+    if (a + b > c && a + c > b && b + c > a) {Console.Write("Числа являются сторонами треугольника.");}
+    else {Console.Write("Числа не являются сторонами треугольника");}
+}
+
+
+
+int A = Sides();
+int B = Sides();
+int C = Sides();
+Console.WriteLine();
+Triangle(A, B, C);
