97e83b611b09a22fa08ae71c31e22c76c40854ec
Alex.link <alex.link_mail@e1.ru>
2021-12-15 23:04:23 +0500

    add Example 045

---
+++ Example045/Program.cs
@@ -0,0 +1,14 @@
+﻿// Показать числа Фибоначчи
+
+int number1 = 0;
+int number2 = 0;
+int number3 = 1;
+int length = 10;
+for (int i = 0; i < length; i++)
+{
+    number1 = number2;
+    number2 = number3;
+    Console.Write($"{number3}, ");
+    number3 = number3 + number1;
+}
+Console.Write($"{number3}.");
