9fce7bbd1e2a18c685a4c3b40f11d205be4fbc57
Alex.link <alex.link_mail@e1.ru>
2021-12-16 13:35:48 +0500

    add Example 048

---
+++ Example048/Program.cs
@@ -0,0 +1,13 @@
+﻿// Показать двумерный массив размером m×n заполненный целыми числами.
+int m = new Random().Next(1, 10);
+int n = new Random().Next(1, 10);
+int[,] matrix = new int[m, n];
+for (int i = 0; i < m; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        matrix[i, j] = new Random().Next(1, 10);
+        Console.Write($"{matrix[i, j]} ");
+    }
+    Console.WriteLine();
+}
