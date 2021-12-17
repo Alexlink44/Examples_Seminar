7c8b0054077fee7cbe705894fc712377269b6817
Alex.link <alex.link_mail@e1.ru>
2021-12-17 18:13:03 +0500

    add Example 053

---
+++ Example053/Program.cs
@@ -0,0 +1,52 @@
+﻿// В двумерном массиве показать позиции числа, заданного пользователем или указать, что такого элемента нет.
+
+int[,] Matrix(int m, int n)
+{
+    int[,] matrix = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            matrix[i, j] = new Random().Next(1, 10);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FindNumber(int[,] matrix, int number)
+{
+    int k = 0;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (matrix[i, j] == number)
+            {
+                Console.WriteLine($"Позиция искомого числа: [{j + 1}, {i + 1}]");
+                k++;
+            }
+            if (k == 1) break;
+        }
+        if (k == 1) break;
+    }
+    if (k != 1) {Console.WriteLine("Искомое число не найдено");}
+}
+
+int[,] matrix = Matrix(5, 6);
+PrintMatrix(matrix);
+
+Console.Write("Введите искомое число: ");
+int number = int.Parse(Console.ReadLine());
+FindNumber(matrix, number);
