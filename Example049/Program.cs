ece1076e14732704eae884b86f62fcde132be28d
Alex.link <alex.link_mail@e1.ru>
2021-12-17 14:32:16 +0500

    add Example 049

---
+++ Example049/Program.cs
@@ -0,0 +1,15 @@
+﻿// Показать двумерный массив размером m×n заполненный вещественными числами
+
+int m = 5;
+int n = 5;
+double[,] matrix = new double[m, n];
+for (int i = 0; i < m; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        double number = new Random().Next(-100, 100);
+        matrix[i, j] = number / 5;
+        Console.Write($"{matrix[i, j]} ");
+    }
+    Console.WriteLine();
+}
