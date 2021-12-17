019445d7a910bc3b5bede0990860526a8ce7c164
Alex.link <alex.link_mail@e1.ru>
2021-12-17 17:14:43 +0500

    add Example 051

---
+++ Example051/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задать двумерный массив следующим правилом: Aₘₙ = m+n.
+
+int m = new Random().Next(4, 7);
+int n = new Random().Next(4, 7);
+int[,] matrix = new int[m, n];
+for (int i = 0; i < m; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        matrix[i, j] = i + j + 2;
+        Console.Write($"{matrix[i, j]}  ");
+    }
+    Console.WriteLine();
+}
