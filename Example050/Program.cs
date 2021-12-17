f1e2d01d85f127daacb5e6118ee230b6adaeb808
Alex.link <alex.link_mail@e1.ru>
2021-12-17 16:58:56 +0500

    add Example 050

---
+++ Example050/Program.cs
@@ -0,0 +1,51 @@
+﻿// В двумерном массиве n×k заменить четные элементы на противоположные
+
+int[,] Matrix(int n, int k)
+{
+    int[,] matrix = new int[n, k];
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < k; j++)
+        {
+            matrix[i, j] = new Random().Next(1, 10);
+        }
+        Console.WriteLine();
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] ReverseMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if ((i + j) % 2 == 0) {matrix[i, j] = matrix[i, j] * (-1);}
+        }
+    }
+    return matrix;
+}
+
+
+
+
+
+int n = new Random().Next(3, 6);
+int k = new Random().Next(3, 6);
+
+int[,] matrix = Matrix(n, k);
+PrintMatrix(matrix);
+matrix = ReverseMatrix(matrix);
+PrintMatrix(matrix);
