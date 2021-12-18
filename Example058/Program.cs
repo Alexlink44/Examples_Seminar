005380b94c871705db1a69dfb6840aa041c4b512
Alex.link <alex.link_mail@e1.ru>
2021-12-18 23:53:37 +0500

    add Example 058

---
+++ Example058/Program.cs
@@ -0,0 +1,49 @@
+﻿// Написать программу, которая в двумерном массиве заменяет строки на столбцы
+// или сообщить, что это невозможно (в случае, если матрица не квадратная).
+
+int[,] Matrix(int m, int n)
+{
+    int[,] matrix = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            matrix[i, j] = new Random().Next(1, 10);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] ReplaceRowsColumns(int[,] matrix)
+{
+    int temp = 0;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = i; j < matrix.GetLength(1); j++)
+        {
+            temp = matrix[i, j];
+            matrix[i, j] = matrix[j, i];
+            matrix[j, i] = temp;
+        }
+    }
+    return matrix;
+}
+
+int[,] matrix = Matrix(5, 5);
+PrintMatrix(matrix);
+Console.WriteLine();
+if (matrix.GetLength(0) == matrix.GetLength(1))
+{matrix = ReplaceRowsColumns(matrix); PrintMatrix(matrix);}
+else {Console.Write("Матрица не квадратная. Заменить строки на столбцы невозможно.");}
