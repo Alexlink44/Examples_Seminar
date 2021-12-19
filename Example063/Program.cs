f5ac233af1d90731bf00e81981cd9d59252536a8
Alex.link <alex.link_mail@e1.ru>
2021-12-19 22:20:52 +0500

    add Example 063

---
+++ Example063/Program.cs
@@ -0,0 +1,17 @@
+﻿// Сформировать трехмерный массив не повторяющимися двузначными числами 
+//показать его построчно на экран выводя индексы соответствующего элемента.
+
+string[,,] array = new string[3,3,3];
+int A = 0;
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        for (int k = 0; k < array.GetLength(2); k++)
+        {
+            if(A < 10) {array[i, j, k] = "0" + Convert.ToString(A); A++;}
+            else {array[i, j, k] = Convert.ToString(A); A++;}
+            Console.WriteLine($"{array[i, j, k]} ({i + 1},{j + 1},{k + 1})");
+        }
+    }
+}
