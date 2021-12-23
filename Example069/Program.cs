35f5baacf65b6387afc288db322df264ae538b5f
Alex.link <alex.link_mail@e1.ru>
2021-12-23 11:48:15 +0500

    add Example 069

---
+++ Example069/Program.cs
@@ -0,0 +1,10 @@
+﻿// Рекурсия.
+// Найти сумму элементов от M до N, N и M заданы.
+
+void SumMN(int M, int N, int sum)
+{
+    if (M <= N) {SumMN(M + 1, N, sum + M);}
+    else {Console.WriteLine(sum);}
+}
+
+SumMN(1, 5, 0);
