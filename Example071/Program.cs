48f5e40ca92a5f9cd6f6b19c26766e83e9c07bf5
Alex.link <alex.link_mail@e1.ru>
2021-12-23 22:42:41 +0500

    add Example 071 Beta

---
+++ Example071/Program.cs
@@ -0,0 +1,20 @@
+﻿// Рекурсия.
+// Написать программу вычисления функции Аккермана.
+
+int Ackermann(int m, int n)
+{
+    if (m > 0 && n > 0)
+    {
+        return Ackermann((m - 1), (Ackermann(m, (n - 1))));
+    }
+    else if (m > 0 && n == 0)
+    {
+        return Ackermann((m - 1), 1);
+    }
+    else
+    {
+        return (n + 1);
+    }
+}
+
+Console.WriteLine(Ackermann(4, 1));
