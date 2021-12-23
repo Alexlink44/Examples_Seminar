35df815d4c34721b005f60a0b747e766b86303da
Alex.link <alex.link_mail@e1.ru>
2021-12-23 13:13:20 +0500

    fix

--- Example068/Program.cs
+++ Example068/Program.cs
@@ -3,7 +3,9 @@
 
 void MNNumbers(int M, int N)
 {
-    if (M <= N) {Console.WriteLine(M); MNNumbers(M + 1, N);}
+    if (M < N) {Console.WriteLine(M); MNNumbers(M + 1, N);}
+    else if (N < M) {Console.WriteLine(N); MNNumbers(N + 1, M);}
+    else {Console.WriteLine(N);}
 }
 
-MNNumbers(5, 20);
+MNNumbers(20, 5);
--- Example070/Program.cs
+++ Example070/Program.cs
@@ -1,10 +1,10 @@
 ﻿// Рекурсия.
 //Найти сумму цифр числа.
 
-int SumNumber(int number, int result)
+int SumNumber(int number)
 {
-    if (number > 0) {result = SumNumber(number / 10, result) + (number % 10); return result;}
+    if (number > 0) {return SumNumber(number / 10) + (number % 10);}
     else return 0;
 }
 
-Console.WriteLine(SumNumber(123, 0));
+Console.WriteLine(SumNumber(123));
