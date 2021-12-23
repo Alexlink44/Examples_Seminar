279b2af84d5fa24cd5cb459fc539ab454be52be2
Alex.link <alex.link_mail@e1.ru>
2021-12-23 11:40:03 +0500

    add Example 068

---
+++ Example068/Program.cs
@@ -0,0 +1,9 @@
+﻿// Рекурсия.
+// Показать натуральные числа от M до N, N и M заданы.
+
+void MNNumbers(int M, int N)
+{
+    if (M <= N) {Console.WriteLine(M); MNNumbers(M + 1, N);}
+}
+
+MNNumbers(5, 20);
