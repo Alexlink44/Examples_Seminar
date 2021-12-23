78a85efb0b30148ea231860a6c50c425a2432af3
Alex.link <alex.link_mail@e1.ru>
2021-12-23 23:07:27 +0500

    add Example 072

---
+++ Example072/Program.cs
@@ -0,0 +1,13 @@
+﻿// Рекурсия.
+//Написать программу возведения числа А в целую стень B.
+
+double Exponentiation(double A, int B)
+{
+    if (B > 0) return Exponentiation(A, B - 1) * A;
+    else
+    {
+        return 1;
+    }
+}
+
+Console.Write(Exponentiation(3, 3));
