28a83765635d589bb1afbee8aeaf9697df80a387
Alex.link <alex.link_mail@e1.ru>
2021-12-24 23:12:00 +0500

    Example 074 v2

--- Example074/Program.cs
+++ Example074/Program.cs
@@ -2,40 +2,60 @@
 //В некотором машинном алфавите имеются четыре буквы «а», «и», «с» и «в».
 //Покажите все слова, состоящие из n букв, которые можно построить из букв этого алфавита.
 
-string[] alfabet = { "а", "и", "с", "в" };
+char[] alfabet = { 'а', 'и', 'с', 'в' };
 
 // Общий вид.
-int wrdLnth = 4;
-int count = alfabet.Length;
-int[] wordsNum = new int[wrdLnth];
-int m = 0;
-int num = 1;
-while (wordsNum[0] < count)
+// int wrdLnth = 4;
+// int count = alfabet.Length;
+// int[] wordsNum = new int[wrdLnth];
+// int m = 0;
+// int num = 1;
+// while (wordsNum[0] < count)
+// {
+//     m = wrdLnth - 2;
+//     for (int i = 0; i < count; i++)
+//     {
+//         wordsNum[wrdLnth - 1] = i;
+//         Console.Write($"{num}: ");
+//         num++;
+//         for (int j = 0; j < wrdLnth; j++)
+//         {
+//             Console.Write(alfabet[wordsNum[j]]);
+//         }
+//         Console.WriteLine();
+//     }
+//     wordsNum[m] = wordsNum[m] + 1;
+//     if (wordsNum[m] == count)
+//     {
+//         while (wordsNum[m] == count)
+//         {
+//             if (m > 0)
+//             {
+//                 wordsNum[m] = 0;
+//                 m--;
+//                 wordsNum[m] = wordsNum[m] + 1;
+//             }
+//             else {wordsNum[m] = wordsNum[m] + 1;}
+//         }
+//     }
+// }
+
+// Через рекурсию.
+
+int n = 1;
+void Words(char[] alfabet, int lnthwrd, string word = "", int length = 0)
 {
-    m = wrdLnth - 2;
-    for (int i = 0; i < count; i++)
+    if (length == lnthwrd)
     {
-        wordsNum[wrdLnth - 1] = i;
-        Console.Write($"{num}: ");
-        num++;
-        for (int j = 0; j < wrdLnth; j++)
-        {
-            Console.Write(alfabet[wordsNum[j]]);
-        }
-        Console.WriteLine();
+        Console.WriteLine($"{n++}: {word}"); return;
     }
-    wordsNum[m] = wordsNum[m] + 1;
-    if (wordsNum[m] == count)
+    else
     {
-        while (wordsNum[m] == count)
+        for (int i = 0; i < alfabet.Length; i++)
         {
-            if (m > 0)
-            {
-                wordsNum[m] = 0;
-                m--;
-                wordsNum[m] = wordsNum[m] + 1;
-            }
-            else {wordsNum[m] = wordsNum[m] + 1;}
+           Words(alfabet, lnthwrd, word + alfabet[i], length + 1);
         }
     }
 }
+
+Words(alfabet, 2);
