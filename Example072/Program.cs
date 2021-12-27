4adb7358ceaa2c1b10288b33bac8d1ed98ba274b
Alex.link <alex.link_mail@e1.ru>
2021-12-27 13:52:11 +0500

    fix Example072

--- Example072/Program.cs
+++ Example072/Program.cs
@@ -4,10 +4,11 @@
 double Exponentiation(double A, int B)
 {
     if (B > 0) return Exponentiation(A, B - 1) * A;
+    else if (B < 0) return Exponentiation(A, B + 1) / A;
     else
     {
         return 1;
     }
 }
 
-Console.Write(Exponentiation(3, 3));
+Console.Write(Exponentiation(2, -3));
---
+++ Task001/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 1
+// ---
+// Подсчитать количество натуральных чисел, не превосходящих заданного числа n, которые
+// - делятся на k, но не на l
+// - делятся хотябы на k или на l
+// - не делятся на (k + l)
+
+Console.Write("Задайте число-ограничение n: ");
+int numLimit = int.Parse(Console.ReadLine());
+int k = 3;
+int l = 7;
+
+int DivisibleByKButNotL(int numLimit, int k, int l)
+{
+    int result = 1;
+    for (int i = 1; i < numLimit; i++)
+    {
+        if (i % k == 0 && i % != 0) {}
+    }
+    return
+}
